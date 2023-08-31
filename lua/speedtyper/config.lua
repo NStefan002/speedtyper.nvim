4e5776fd403c440814dc882b37712e0b51fa6cc3
NStefan002 <n.stefan002@gmail.com>
2023-08-31 03:11:33 +0200

    fix

--- lua/speedtyper/config.lua
+++ lua/speedtyper/config.lua
@@ -3,19 +3,19 @@ local M = {}
 ---@type table<string, any>
 M.default_opts = {
     window = {
-        height = 5,         -- integer >= 5 | float in range (0, 1)
-        width = 0.55,       -- integer | float in range (0, 1)
+        height = 5, -- integer >= 5 | float in range (0, 1)
+        width = 0.55, -- integer | float in range (0, 1)
         border = "rounded", -- "none" | "single" | "double" | "rounded" | "shadow" | "solid"
     },
-    language = "en",        -- currently only only supports English
-    game_modes = {          -- prefered settings for different game modes
+    language = "en", -- currently only only supports English
+    game_modes = { -- prefered settings for different game modes
         -- type until time expires
         countdown = {
             time = 30,
         },
         -- type until you complete one page
         stopwatch = {
-            hide_time = true -- hide time while typing
+            hide_time = true, -- hide time while typing
         },
     },
 }
@@ -26,7 +26,7 @@ M.opts = {}
 ---@param opts table<string, any>
 function M.override_opts(opts)
     M.opts = vim.tbl_deep_extend("force", M.default_opts, opts or {})
-    require("speedtyper.langs").set_lang(opts.language)
+    require("speedtyper.langs").set_lang(M.opts.language)
 end
 
 return M
