c9d88e30051aef2f9e7871a9e4969f7a9b3eb2fe
Branislav Gerazov <gerazov@feit.ukim.edu.mk>
2023-12-30 21:40:10 +0100

    Added English sentence mode for countdown and stopwatch games.

--- lua/speedtyper/game_modes/stopwatch/init.lua
+++ lua/speedtyper/game_modes/stopwatch/init.lua
@@ -67,7 +67,7 @@ function M.create_timer()
     M.timer = (vim.uv or vim.loop).new_timer()
     local extm_id = api.nvim_buf_set_extmark(0, ns_id, n_lines - 1, 0, {
         virt_text = {
-            { "Press 'i' to start the game.", "WarningMsg" },
+            { "Press 'i' to start the game.", "DiagnosticOk" },
         },
         virt_text_pos = "right_align",
     })
--- lua/speedtyper/langs/init.lua
+++ lua/speedtyper/langs/init.lua
@@ -1,10 +1,15 @@
 local M = {}
 local util = require("speedtyper.util")
+local opts = require("speedtyper.config").opts
 
-M.supported_options = {
+M.supported_words = {
     "custom", -- if custom_text option is provided
     "en",
     "sr",
+}
+
+M.supported_sentences = {
+    "en",
 }
 
 M.lang = ""
@@ -12,13 +17,23 @@ M.lang = ""
 ---@param lang string
 function M.set_lang(lang)
     local supported = false
-    for _, l in pairs(M.supported_options) do
+    local supported_langs = {}
+    if opts.sentence_mode then
+        supported_langs = M.supported_sentences
+    else
+        supported_langs = M.supported_words
+    end
+    for _, l in pairs(supported_langs) do
         if lang == l then
             supported = true
         end
     end
     if not supported then
-        util.error("Language " .. lang .. " is not supported!")
+        if opts.sentence_mode then
+            util.error("Language " .. lang .. " is not supported for sentence mode!")
+        else
+            util.error("Language " .. lang .. " is not supported!")
+        end
     else
         M.lang = lang
     end
@@ -28,6 +43,12 @@ end
 ---@return string[]
 function M.get_words()
     return require("speedtyper.langs." .. M.lang)
+end
+
+---get list of sentences for selected language
+---@return string[]
+function M.get_sentences()
+    return require("speedtyper.langs.sentences." .. M.lang)
 end
 
 return M
