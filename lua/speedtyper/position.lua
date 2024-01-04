021ba1b5848c4887233d38abe5af417625c9cb7c
NStefan002 <n.stefan002@gmail.com>
2024-01-04 03:05:11 +0100

    feat: position helper class

---
+++ lua/speedtyper/position.lua
@@ -0,0 +1,34 @@
+---helper class for storing a different currsor/word/char positions
+---@class Position
+---@field line integer
+---@field col integer
+
+local Position = {}
+Position.__index = Position
+
+---@param line integer
+---@param col integer
+function Position.new(line, col)
+    local pos = { line = line, col = col }
+    return setmetatable(pos, Position)
+end
+
+---@param new_line integer
+---@param new_col integer
+function Position:update(new_line, new_col)
+    self.line = new_line
+    self.col = new_col
+end
+
+---@param o Position
+---@param p Position
+---@return boolean
+function Position.equal(o, p)
+    return o.line == p.line and o.col == p.col
+end
+
+function Position:__eq(other)
+    return self:equal(other)
+end
+
+return Position
