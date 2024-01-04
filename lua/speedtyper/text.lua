b53a6c4afe2df10a9018038bcdd316ddfb1d4653
NStefan002 <n.stefan002@gmail.com>
2024-01-04 02:39:28 +0100

    feat: configured text and typotracker

--- lua/speedtyper/text.lua
+++ lua/speedtyper/text.lua
@@ -1,20 +1,29 @@
 local Util = require("speedtyper.util")
 
 ---@class SpeedTyperText
----@field available_langs string[]
 ---@field selected_lang string
 ---@field words string[]
 
-local SpeedTyperText = {
-    available_langs = { "en", "sr" },
-    selected_lang = nil,
-    words = nil,
-}
+local SpeedTyperText = {}
+SpeedTyperText.__index = SpeedTyperText
+
+function SpeedTyperText.new()
+    local text = {
+        selected_lang = "",
+        words = {},
+    }
+    return setmetatable(text, SpeedTyperText)
+end
+
+---@return string[]
+function SpeedTyperText.get_available_langs()
+    return { "en", "sr" }
+end
 
 ---@param lang string
 function SpeedTyperText:set_lang(lang)
     if
-        Util.tbl_contains(self.available_langs, lang, function(a, b)
+        Util.tbl_contains(self.get_available_langs(), lang, function(a, b)
             return a == b
         end)
     then
@@ -31,16 +40,19 @@ function SpeedTyperText:get_word()
     return self.words[math.random(#self.words)]
 end
 
----@param max_len number
+---@param win_width integer
 ---@return string
-function SpeedTyperText:generate_sentence(max_len)
-    local sentence = ""
+function SpeedTyperText:generate_sentence(win_width)
+    local border_width = 2
+    local extra_space = 1 -- at the end of the sentence
+    local usable_width = win_width - 2 * border_width - extra_space -- 2 * border -> left and right border
+    local sentence = self:get_word()
     local word = self:get_word()
-    while #sentence + #word < max_len do
-        sentence = word .. " " .. sentence
+    while #sentence + #word < usable_width do
+        sentence = sentence .. " " .. word
         word = self:get_word()
     end
-    return sentence
+    return sentence .. " "
 end
 
 return SpeedTyperText
--- lua/speedtyper/typo.lua
+++ lua/speedtyper/typo.lua
@@ -1,4 +1,5 @@
 local Util = require("speedtyper.util")
+local Position = require("speedtyper.position")
 
 ---@class SpeedTyperTyposTracker
 ---@field ns_id integer
@@ -6,40 +7,19 @@ local Util = require("speedtyper.util")
 ---@field bufnr integer
 ---@field typos Position[]
 
----@class Position
----@field line integer
----@field col integer
-
-local Position = {}
-
----@param line integer
----@param col integer
-function Position:new(line, col)
-    local pos = setmetatable({ line = line, col = col }, self)
-    self.__index = self
-    return pos
-end
-
----@param o Position
----@param p Position
----@return boolean
-function Position.equal(o, p)
-    return o.line == p.line and o.col == p.col
-end
-
 local SpeedTyperTyposTracker = {}
+SpeedTyperTyposTracker.__index = SpeedTyperTyposTracker
 
 ---@param highlight string
 ---@param bufnr? integer
-function SpeedTyperTyposTracker:new(highlight, bufnr)
-    local typo = setmetatable({
+function SpeedTyperTyposTracker.new(highlight, bufnr)
+    local typo = {
         ns_id = vim.api.nvim_create_namespace("SpeedTyper"),
         typos = {},
         bufnr = bufnr or 0,
         highlight = highlight,
-    }, self)
-    self.__index = self
-    return typo
+    }
+    return setmetatable(typo, SpeedTyperTyposTracker)
 end
 
 ---@param should_be string
@@ -48,18 +28,23 @@ function SpeedTyperTyposTracker:check_curr_char(should_be)
         return
     end
     local line, col = Util.get_cursor_pos()
+    if col == 1 then
+        return
+    end
+    col = col - 1
     local typed = vim.api.nvim_buf_get_text(self.bufnr, line - 1, col - 1, line - 1, col, {})[1]
     if typed ~= should_be then
         self.typos = self.typos or {}
-        table.insert(self.typos, Position:new(line, col))
+        table.insert(self.typos, Position.new(line, col))
+        SpeedTyperTyposTracker._mark_typo(self, line, col)
     else
-        Util.remove_element(self.typos, Position:new(line, col), function(a, b)
-            return Position.equal(a, b)
+        Util.remove_element(self.typos, Position.new(line, col), function(a, b)
+            return a == b
         end)
     end
 end
 
-function SpeedTyperTyposTracker:mark_typo(line, col)
+function SpeedTyperTyposTracker:_mark_typo(line, col)
     vim.api.nvim_buf_add_highlight(self.bufnr, self.ns_id, self.highlight, line - 1, col - 1, col)
 end
 
