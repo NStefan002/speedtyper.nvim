3343482fe541b84afd5c47546a20612d3476b722
NStefan002 <n.stefan002@gmail.com>
2024-07-02 23:36:50 +0200

    feat(globals): add types

--- lua/speedtyper/globals.lua
+++ lua/speedtyper/globals.lua
@@ -1,10 +1,13 @@
 local M = {}
 local api = vim.api
 
+---@type integer
 M.ns_id = api.nvim_create_namespace("SpeedTyper")
 
+---@type integer
 M.bufnr = -1
 
+---@type integer
 M.winnr = -1
 
 return M
