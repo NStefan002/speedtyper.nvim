ad1c89f3a6761982a7cba6170a930b31e3a5df79
NStefan002 <n.stefan002@gmail.com>
2024-12-01 14:13:13 +0100

    feat(util): more functions for working with utf strings

--- lua/speedtyper/util.lua
+++ lua/speedtyper/util.lua
@@ -313,6 +313,7 @@ end
 ---Get the character at the given index in a utf-8 string
 ---@param str string
 ---@param idx integer if negative, index from the end of the string (-1 is the last character)
+---@return string
 function M.utf_char_at(str, idx)
     local utf_indices = vim.str_utf_pos(str)
     if idx == 0 or idx > #utf_indices then
@@ -322,6 +323,55 @@ function M.utf_char_at(str, idx)
         idx = #utf_indices + idx + 1
     end
     return str:sub(utf_indices[idx], idx < #utf_indices and utf_indices[idx + 1] - 1 or -1)
+end
+
+---find the index of a character in a utf-8 string
+---@param str string utf-8 string
+---@param char string character to search for
+---@param start? integer index to start searching from (indicies are the positions of utf-8 characters, not bytes as in default lua strings)
+---@return integer
+function M.utf_find(str, char, start)
+    if api.nvim_strwidth(char) ~= 1 then
+        M.error("Only single-char strings are supported")
+        return -1
+    end
+    local utf_indices = vim.str_utf_pos(str)
+    local start_idx = utf_indices[start] or 1
+    local char_idx, _, _ = str:find(char, start_idx, true)
+    if char_idx == nil then
+        return -1
+    end
+    for i, idx in ipairs(utf_indices) do
+        if idx == char_idx then
+            return i
+        end
+    end
+    return -1
+end
+
+---get a substring of a utf-8 string
+---@param str string
+---@param start integer index of the first character of the substring (indicies are the positions of utf-8 characters, not bytes as in default lua strings)
+---@param stop? integer if nil, the substring will be from `start` to the end of the string
+---@return string
+function M.utf_sub(str, start, stop)
+    local utf_indices = vim.str_utf_pos(str)
+
+    if start > #utf_indices then
+        return ""
+    end
+    if start < 0 then
+        start = #utf_indices + start + 1
+    end
+    local start_idx = utf_indices[start] or 1
+
+    stop = stop or #utf_indices
+    if stop < 0 then
+        stop = #utf_indices + stop + 1
+    end
+    local stop_idx = (utf_indices[stop + 1] or 0) - 1
+
+    return str:sub(start_idx, stop_idx)
 end
 
 return M
