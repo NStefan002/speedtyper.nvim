922c6df5fbaa0beb5f48aded6e769eff7e5df4e3
NStefan002 <n.stefan002@gmail.com>
2024-12-03 00:46:25 +0100

    fix(util): use `nvim_strwidth` instead of `#`

--- lua/speedtyper/util.lua
+++ lua/speedtyper/util.lua
@@ -226,7 +226,8 @@ end
 
 ---@param text string
 function M.center_text(text, buff_width)
-    local sep = string.rep(" ", math.floor((buff_width - #text) / 2))
+    local text_len = api.nvim_strwidth(text)
+    local sep = string.rep(" ", math.floor((buff_width - text_len) / 2))
     return string.format("%s%s%s", sep, text, sep)
 end
 
